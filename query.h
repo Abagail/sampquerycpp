abffaaa1c6fad888bff4b6bc59731e5e2d5a76b4
Abagail <evanabagail@gmail.com>
2015-03-31 17:24:11 -0400

    Adds some missing linux headers
    Credits to ranchos

--- query.h
+++ query.h
@@ -2,17 +2,14 @@
 	C++ Implementation of the SA-MP RCON/Query mechanism.
 	
     Copyright (C) 2013 Cody Cunningham
-
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -24,6 +21,7 @@
 #ifdef WIN32
 #include <windows.h>
 #include <winsock.h>
+
 #pragma comment(lib, "Ws2_32.lib")
 
 typedef int socklen_t;
@@ -35,6 +33,8 @@ typedef int socklen_t;
 
 #include <stdlib.h>
 #include <fcntl.h>
+#include <netdb.h>
+#include <unistd.h>
 
 typedef unsigned char BYTE;
 #define closesocket close
